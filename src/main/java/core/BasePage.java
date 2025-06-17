bd2ef93a0ceb792eecb0e06d6fb7d084bce0d7fe
TrinhToThi <trinhto.qa@gmail.com>
2025-06-17 22:39:56 +0700

    BasePage WebBrowser

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -1,4 +1,138 @@
 package core;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
+
 public class BasePage {
+
+    //1- Access modifier:
+    // public: Tất cả các class trong cùng/ khác package đều có thể sử dụng các hàm ny
+    // protected: chỉ class nào kế thừa mới được dùng
+    // private: Chỉ cho hàm trong cùng class dùng
+    // default: chỉ cho các class trong cùng package dùng
+
+    // 2- Kiểu dữ lệu của hàm (data type): void/ int/ string/ boolean/ webelement/ list<WebElement>/...
+    // Nó s liên quan đến chức năng mình vết trong thân hàm
+    // Dùng cái hàm nào của selenium thì nó trả về cái gì -> define theo kiểu dữ liệu của hàm tương ứng
+
+    // 3- Tên hàm: Đặt tên có nghĩa theo chức năng đang cần viết (lowerCamelCase)
+    // 4-  có tham số hay không?
+    // dùng theo hàm của selenium (thao tác với cái gì truyền tham số đấy)
+
+    //5- Kiểu dữ liệu trả về cho hàm
+    // Nếu như có return dữ liệu thì sẽ khớp với kiểu dữ liệu ở số 2
+    // Nếu như không có return thì nó là cái step cuối cùng
+
+
+    // Viết 1 hàm getText của 1 element bất kỳ
+
+    public void openPageURL(WebDriver driver, String pageUrl) {
+        driver.get(pageUrl);
+    }
+
+    public String getPageTitle(WebDriver driver) {
+        return driver.getTitle();
+        // ngoại trừ void, tất cả còn lại cần return
+    }
+
+    public String getPageUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageSource(WebDriver driver) {
+        return driver.getPageSource();
+    }
+
+    public void forwardToPage(WebDriver driver) {
+        driver.navigate().forward();
+    }
+
+    public void refreshPage(WebDriver driver) {
+        driver.navigate().refresh();
+    }
+
+    private Alert waitAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void acceptToAlert(WebDriver driver) {
+        waitAlertPresence(driver).accept();
+    }
+
+    public void cancelToAlert(WebDriver driver) {
+        waitAlertPresence(driver).dismiss();
+    }
+
+    public void sendKeyToAlert(WebDriver driver, String keyToSend) {
+        waitAlertPresence(driver).sendKeys(keyToSend);
+    }
+
+    public String getAlertText(WebDriver driver) {
+        return waitAlertPresence(driver).getText();
+    }
+
+    public void sleepInSecond(int timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void switchToWindowByID(WebDriver driver, String windowID) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String window : allWindows) {
+            if (!window.equals(windowID)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            driver.switchTo().window(window);
+            String pageTitle = driver.getTitle();
+            if (pageTitle.equals(expectedTitle)) {
+                break;
+            }
+        }
+        sleepInSecond(2);
+    }
+
+    public void switchToWindowByContainTitle(WebDriver driver, String expectedTitle) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            driver.switchTo().window(window);
+            if (driver.getTitle().contains(expectedTitle)) {
+                break;
+            }
+        }
+        sleepInSecond(2);
+    }
+
+    public void closeAllExceptMain(WebDriver driver, String windowID) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            if (!window.equals(windowID)) {
+                driver.switchTo().window(window);
+                driver.close();
+            }
+        }
+        driver.switchTo().window(windowID);
+    }
 }
+
